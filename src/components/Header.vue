34a51a2bd03ac536353270e545eeb502b4f8b315
gcnova <zhangnova927@foxmail.com>
2022-08-21 19:19:12 +0800

    feat:大概布局

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -6,8 +6,6 @@
   </div>
 </template>
 
-<script setup lang="ts"></script>
-
 <style scoped lang="less">
 .header {
   margin: 0 auto;
--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,6 +1,36 @@
 <!-- Home -->
 <template>
-  <div></div>
+  <div class="main">
+    <div class="top">
+      <div class="p1"></div>
+      <div class="p2">
+        <div class="c1"></div>
+        <div class="c2"></div>
+        <!-- 饼图 -->
+        <div class="c3"></div>
+        <div class="c4">
+          <div class="dec">
+            <div class="number"></div>
+            <div class="number"></div>
+          </div>
+        </div>
+      </div>
+      <div class="right-box">
+        <div class="p3"></div>
+        <div class="p4"></div>
+      </div>
+    </div>
+    <div class="bototm">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  </div>
 </template>
 
 <script setup>
