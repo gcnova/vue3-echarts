a4661204a2b57635f657182111cbcdc9816049e8
gcnova <zhangnova927@foxmail.com>
2022-08-22 03:30:23 +0800

    feat:基本布局

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -10,8 +10,14 @@
         <div class="c3"></div>
         <div class="c4">
           <div class="dec">
-            <div class="number"></div>
-            <div class="number"></div>
+            <div class="number">
+              <p class="title">公司应到人数</p>
+              <p class="numb">500</p>
+            </div>
+            <div class="number">
+              <p class="title">部门应到人数</p>
+              <p class="numb">500</p>
+            </div>
           </div>
         </div>
       </div>
@@ -21,19 +27,46 @@
       </div>
     </div>
     <div class="bototm">
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
+      <div class="b1">
+        <div class="bg"></div>
+        <div class="tag"></div>
+      </div>
+      <div class="b2">
+        <div class="bg1"></div>
+        <div class="bg2"></div>
+        <div class="tag"></div>
+      </div>
+      <div class="b3">
+        <div class="bg1"></div>
+        <div class="bg2"></div>
+        <div class="tag"></div>
+      </div>
+      <div class="b4">
+        <div class="bg1"></div>
+        <div class="bg2"></div>
+        <div class="tag"></div>
+      </div>
+      <div class="b5">
+        <div class="bg"></div>
+        <div class="tag"></div>
+      </div>
+      <div class="b6">
+        <div class="bg"></div>
+        <div class="tag"></div>
+      </div>
+      <div class="b7">
+        <div class="bg"></div>
+        <div class="tag"></div>
+      </div>
+      <div class="b8">
+        <div class="bg"></div>
+        <div class="tag"></div>
+      </div>
     </div>
   </div>
 </template>
 
-<script setup>
+<script setup lang="ts">
 </script>
 <style lang="less" scoped>
 @import url("./index.less");
